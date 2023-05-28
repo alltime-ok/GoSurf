7dd9c0f4bd7ee8c70a3f6eb3aba1e59ae4c0c19f
alltime-ok <igor.duda6666@gmail.com>
2023-05-29 01:55:08 +0300

    surf slider

--- app/js/main.js
+++ app/js/main.js
@@ -4,12 +4,28 @@ $(function () {
     fade: true,
     prevArrow: '<img class="slider-arrows slider-arrows_left" src="images/src/arrow-left.svg" alt="arrow" />',
     nextArrow: '<img class="slider-arrows slider-arrows_right" src="images/src/arrow-right.svg" alt="arrow" />',
-    asNavFor: ".slider-dots",
+    asNavFor: ".slider-dotshead",
   });
 
-  $(".slider-dots").slick({
+  $(".slider-dotshead").slick({
     slidesToShow: 4,
     slidesToScroll: 4,
     asNavFor: ".header_slider",
+  });
+
+  $(".surf-slider").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '<img class="slider-arrows slider-arrows_left" src="images/src/arrow-left.svg" alt="arrow" />',
+    nextArrow: '<img class="slider-arrows slider-arrows_right" src="images/src/arrow-right.svg" alt="arrow" />',
+    asNavFor: ".slider-map",
+  });
+
+  $(".slider-map").slick({
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    arrows: false,
+    asNavFor: ".surf-slider",
+    focusOnSelect: true,
   });
 });
